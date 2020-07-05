d7a2d73748eaa458cae47483fb4365cafbbcd785
Kush Gosalia <gosaliakush@gmail.com>
2020-07-05 14:39:10 +0530

    Some UI

---
+++ app/src/main/java/android/example/medicinescheduerapp/ui/LoginViewModel.java
@@ -0,0 +1,7 @@
+package android.example.medicinescheduerapp.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/android/example/medicinescheduerapp/ui/SignupViewModel.java
@@ -0,0 +1,7 @@
+package android.example.medicinescheduerapp.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignupViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
